a879530bc26d5c60fd0523de615451234082c880
Joel Júnior <joelviana250@gmail.com>
2025-05-26 16:52:52 -0300

    resolvido o desafio: menor e maior numero

---
+++ js/encontrarMaiorNumero.js
@@ -0,0 +1,11 @@
+"use strict";
+// Desafio: Encontrar o menor e o maior número de um array(sem usar Math.min/Math.max)
+const encontraMaiorNumero = (array) => {
+    const numerosOrdenados = array.sort((a, b) => b - a);
+    return `Maior ${numerosOrdenados[0]}, menor ${numerosOrdenados[numerosOrdenados.length - 1]}`;
+};
+console.log(encontraMaiorNumero([9, 1, 3, 98, 1032]));
+/* Melhoria:
+    Não modificar o array original:
+    const numerosOrdenados = [...array].sort((a, b) => b - a);
+*/
