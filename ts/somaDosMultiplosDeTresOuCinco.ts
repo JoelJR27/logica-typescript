656256ac4a8055f328a3ef9f91c1302837b60a97
Joel Júnior <joelviana250@gmail.com>
2025-06-26 12:04:34 -0300

    feat(ts): desafio 'somar os múltiplos de 3 e 5 entre 1 e n' resolvido

---
+++ ts/somaDosMultiplosDeTresOuCinco.ts
@@ -0,0 +1,14 @@
+// Desafio: Crie uma função que some todos os números de 1 até n que sejam múltiplos de 3 ou 5.
+// 1- Receber como parâmetro um número n;
+// 2- Separar todos os números que sejam múltiplos de 3 || 5;
+// 3- Retornar a soma de todos os múltiplos;
+
+function somaMultiplos(numero: number) {
+  const multiplos: number[] = [];
+  for (let index = 1; index < numero; index++) {
+    if (index % 3 === 0 || index % 5 === 0) multiplos.push(index);
+  }
+  return multiplos.reduce((anterior, acumulador) => anterior + acumulador, 0);
+}
+
+console.log(somaMultiplos(10));
