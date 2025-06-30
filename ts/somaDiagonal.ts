f0dcd58a2aaf94bcafae0e8bb6377695127d7764
Joel Júnior <joelviana250@gmail.com>
2025-06-29 21:19:20 -0300

    feat(ts): resolver desafio: 'Soma Diagonal (matriz NxN)'

---
+++ ts/somaDiagonal.ts
@@ -0,0 +1,25 @@
+// Desafio: Dada uma matriz quadrada NxN, calcule a soma da diagonal principal.
+// A matriz principal é dada como: a soma dos elementos de cada array, começando na posição 0 e incrementando seus índices até n-1;
+// 1- Receber como parâmetro um array contendo uma matriz;
+// 2- Criar um array que represente a diagonal principal;
+// 3- Iterar sobre o array recebido;
+// 4- Durante o loop, fazer o push de cada elemento da diagonal principal;
+// 5- Retornar a soma da diagonal principal, usando o método reduce();
+
+type Matriz = number[][];
+function somarDigonalPrincipal(matriz: Matriz) {
+  let soma = 0;
+  for (let indice = 0; indice < matriz.length; indice++) {
+    soma += matriz[indice][indice];
+  }
+  return soma;
+}
+
+console.log(
+  somarDigonalPrincipal([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+  ])
+);
